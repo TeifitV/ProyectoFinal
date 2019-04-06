167814cb3c1ba89867e5b7572c482a2b27a94891
julio13carbajal <juliovcarbajal@gmail.com>
2019-04-06 11:26:52 -0600

    En esta parte se realizo el DatePicker

--- src/components/TaskModal/index.js
+++ src/components/TaskModal/index.js
@@ -1,3 +1,5 @@
+
+
 import React,{ Component } from 'react';
 import {
   View,
@@ -5,17 +7,20 @@ import {
   Text,
   TextInput,
   TouchableHighlight,
+  ImageBackground,
   StyleSheet,
   Platform
 } from 'react-native';
 
 import Colors from './../../theme/colors';
-
+import DatePicker from 'react-native-datepicker';
 export default class TaskModal extends Component{
   constructor(props){
     super(props)
+    currentDate=new Date()
     this.state={
-      taskText:''
+      taskText:'',
+      date: currentDate
     }
   }
 
@@ -26,23 +31,48 @@ export default class TaskModal extends Component{
       //TODO: show an alert to the user!
     }
   }
-  
+
   render() {
 
     return (
-      <View style={styles.modalContainer}>
+     <View style={styles.modalContainer}>
         <View style={styles.wrapperContainer}>
-          <View style={styles.headerContainer}>
-            <Image
-              style={styles.remindMeLogo}
-              source={require('./../../images/remindMe-lLogo.png')}/>
-          </View>
+          <ImageBackground source={require('./../../images/pattern.png')}  style={styles.headerContainer}>
+            <Text style={styles.headerText}>   AGREGAR TAREA</Text >
+
+              </ImageBackground>
           <View style={styles.bodyContainer}>
-              <Text style={styles.addTaskText}>Agregar Tarea</Text>
+              <Text style={styles.addTaskText}></Text>
               <TextInput
                 style={styles.inputText}
-                placeholder="Nombre de la tarea"
+                placeholder="Agregar Tarea"
                 onChangeText={ (text) => this.setState({taskText: text}) }/>
+                
+               <DatePicker
+                style={{width: 300}}
+                date={this.state.date}
+                mode="date"
+                format="DD-MMM-YYYY"
+                androidMode="spinner"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                showIcon={false}
+                customStyles={{
+                  dateIcon: {
+                    position: 'center',
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0
+                  },
+                  dateInput: {
+                    borderColor:'transparent',
+
+
+                    marginLeft: 0
+                  }
+                }}
+                onDateChange={(date) => {this.setState({date: date})}}
+              />
               <TouchableHighlight
                 style={styles.addButton}
                 underlayColor={Colors.touchableHover}
@@ -62,8 +92,9 @@ export default class TaskModal extends Component{
       </View>
     );
   }
-
 }
+
+
 
 // TODO: Create Theme styles (Buttons, Global Components, etc) so it can be imported on each component as required.
 const styles = StyleSheet.create({
@@ -75,32 +106,39 @@ const styles = StyleSheet.create({
   },
   wrapperContainer: {
     backgroundColor: 'white',
-    height: 567
+    height: 567,
   },
   headerContainer:{
+    width:'100%',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: Colors.primaryColorDarker
+
   },
-  remindMeLogo:{
-    width: 285,
+  headerText:{
+    width: 300,
+    fontFamily: 'Roboto',
+    color: Colors.white,
+    fontSize: 30,
+    textAlign: 'center'
+   
   },
   bodyContainer: {
     flex: 1,
     height: 315,
     justifyContent: 'space-around',
     alignItems: 'center',
-    padding: 20
+    padding: 20,
   },
   addTaskText: {
-    fontFamily: 'Avenir',
+    fontFamily: 'Roboto',
     fontSize: 16,
     textAlign: 'center'
   },
   inputText:{
     width: 300,
-    fontFamily: 'Avenir',
+    fontFamily: 'Roboto',
     fontSize: 30,
     textAlign: 'center',
     borderBottomWidth: Platform.OS === 'ios' ? 2 : 0,
@@ -117,7 +155,7 @@ const styles = StyleSheet.create({
       borderColor: Colors.primaryColorDarker
   },
   addButtonText: {
-    fontFamily: 'Avenir',
+    fontFamily: 'Roboto',
     textAlign: 'center',
     fontSize: 17,
     color: 'white'
@@ -133,7 +171,7 @@ const styles = StyleSheet.create({
       borderColor: Colors.primaryColorDarker
   },
   cancelButtonText: {
-    fontFamily: 'Avenir',
+    fontFamily: 'Roboto',
     textAlign: 'center',
     fontSize: 17,
     color: Colors.primaryColorDarker
